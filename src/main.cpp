c7f2f51afe2de88a2e1effec3f557ba489e07e10
hchtr <155396759+hchtr@users.noreply.github.com>
2025-12-13 19:26:26 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <limits>
 
+int main() {
+  std::cout << "Hello World!\n";
+
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cin.get();
+  
+  return 0;
+}
